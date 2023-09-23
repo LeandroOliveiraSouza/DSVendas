e4ee280b301df99c935dbc71b6715badf1e4c0a4
LeandroOliveiraSouza <leandro_oliveirasouza@outlook.com>
2023-09-23 11:17:15 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,13 @@
+import DataTable from "./components/DataTable";
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+
 export default function App() {
   return (
     <>
-      <h1 className="text-primary">Hello World</h1>
+      <NavBar />
+      <DataTable />
+      <Footer />
     </>
   );
 }
